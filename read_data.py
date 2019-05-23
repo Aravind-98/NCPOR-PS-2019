725072d92f011ac15f303a45e18364e5c8408531
Manan <manansoni0@gmail.com>
2019-05-23 13:38:49 +0530

    Finish plotting 1 nc file's data using basemap

--- read_data.py
+++ read_data.py
@@ -1,47 +1,39 @@
 from netCDF4 import Dataset
 from pprint import pprint
 import numpy as np
+import numpy.ma as ma
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import matplotlib.cm as cm
-path = './seaice_conc_monthly_sh_f08_198903_v03r01.nc'
+path = './seaice_conc_monthly_sh_f17_201712_v03r01.nc'
 data = Dataset(path,'r+')
 
-## plot sea ice fraction in p.i.b. on a given month
-def plot_ice(name):
-    ## read the data
-    fh = Dataset(path, mode='r')
-    lon = fh.variables['longitude'][:]
-    lat = fh.variables['latitude'][:]
-    sic = fh.variables[name][:]
-    fh.close()
+for var in data.variables:
+    print(var)
+print('\n')
 
-    ## plot sea ice concentration
-    fig = plt.figure(figsize=[8,6])
-    m = Basemap(projection='stere',resolution='h',lat_0=-64,lon_0=0,\
-                llcrnrlon=-80,urcrnrlon=80,llcrnrlat=-39.36,urcrnrlat=-89.84)
-    m.drawmapboundary(fill_color='white')
-    m.drawcoastlines()
-    m.fillcontinents()
-    m.drawparallels(np.arange(-89.,-60.,2.),labels=[1,0,0,0],fontsize=14)
-    m.drawmeridians(np.arange(-180.,181.,10.),labels=[0,0,0,1],fontsize=14)
-    xi, yi = m(lon, lat)
+def plot(lat,lon,data):
+    # make South pole Stereographic projection
+    m = Basemap(projection='spstere', boundinglat=-30, lon_0=180., resolution='l', round=True)
+    # may not be required
+    m.drawcountries(linewidth=0.25)
+    m.fillcontinents(alpha=0.42)
 
-    frac_levels = np.linspace(0,1,11)
-    cs = m.contourf(xi, yi, sic[0, :, :], frac_levels, cmap=cm.PuBu_r)
-    cbar = plt.colorbar(cs, fraction=0.046, pad=0.04)
-    cbar.ax.tick_params(labelsize=14)
-    cbar.ax.set_ylabel('Sea Ice Conc.', fontsize=14)
-    plt.title('PIB sea ice conc., Summer', fontsize=14)
-    #plt.savefig('./media/ice_img/seaice_conc_.png'.format(date))
+    # draw parallel longitude lines
+    m.drawmeridians(np.arange(-180.,181.,30.), labels=[1,1,1,1])
+
+    # draw parallel latitude lines
+    m.drawparallels(np.arange(-90.,-40.,10.), labels=[1,1,1,1])
+
+    # plot the results
+    m.pcolormesh(lon, lat, data[0,:,:], cmap='RdYlBu',latlon=True)
+    plt.title('South Pole Stereographic Projection for sea ice concentration')
+    plt.colorbar(orientation='horizontal', ticks=np.arange(0,1,10), )
     plt.show()
-    plt.close();
-# pprint(data.variables["seaice_conc_monthly_cdr"])
-# pprint(data.variables["seaice_conc_monthly_cdr"][:])
-ds_meta = data.variables["longitude"]
-ds = ds_meta[:]
-#pprint(ds_meta)
-#pprint(ds[ds.mask==True])
-#pprint(ds.shape)
-#pprint(ds)
-plot_ice('seaice_conc_monthly_cdr')
+
+lat = data.variables["latitude"][:]
+lon = data.variables["longitude"][:]
+
+sic = data.variables["seaice_conc_monthly_cdr"][:]
+sic_masked = ma.masked_less_equal(sic,0.0)
+plot(lat,lon,sic_masked)
