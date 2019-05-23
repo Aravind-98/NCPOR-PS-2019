ab51e83dd57f0549c19f664cb179da613f6e6a4f
Manan <manansoni0@gmail.com>
2019-05-23 14:26:24 +0530

    Finish plotting data for 1 netcdf file (all parameters)

--- read_data.py
+++ read_data.py
@@ -12,12 +12,15 @@ for var in data.variables:
     print(var)
 print('\n')
 
-def plot(lat,lon,data):
+def plot(fig,num,heading,lat,lon,data):
+    ax = fig.add_subplot(f'23{num}')
+    ax.set_title(heading, pad=20)
     # make South pole Stereographic projection
-    m = Basemap(projection='spstere', boundinglat=-30, lon_0=180., resolution='l', round=True)
+    m = Basemap(projection='spstere', boundinglat=-30, lon_0=180., resolution='h', round=True)
+
     # may not be required
-    m.drawcountries(linewidth=0.25)
     m.fillcontinents(alpha=0.42)
+    #ax.title(heading)
 
     # draw parallel longitude lines
     m.drawmeridians(np.arange(-180.,181.,30.), labels=[1,1,1,1])
@@ -25,15 +28,47 @@ def plot(lat,lon,data):
     # draw parallel latitude lines
     m.drawparallels(np.arange(-90.,-40.,10.), labels=[1,1,1,1])
 
-    # plot the results
+    # add the observations to the map
     m.pcolormesh(lon, lat, data[0,:,:], cmap='RdYlBu',latlon=True)
-    plt.title('South Pole Stereographic Projection for sea ice concentration')
-    plt.colorbar(orientation='horizontal', ticks=np.arange(0,1,10), )
-    plt.show()
+
+    #fig.colorbar(cm.ScalarMappable(cmap='RdYlBu'),orientation='horizontal',ax=ax)
 
 lat = data.variables["latitude"][:]
 lon = data.variables["longitude"][:]
+fig = plt.figure()
+
 
 sic = data.variables["seaice_conc_monthly_cdr"][:]
 sic_masked = ma.masked_less_equal(sic,0.0)
-plot(lat,lon,sic_masked)
+plot(fig,1,'Concentration',lat,lon,sic_masked)
+
+std_sic = data.variables["stdev_of_seaice_conc_monthly_cdr"][:]
+std_sic_masked = ma.masked_less_equal(sic,0.0)
+plot(fig,2,'std_dev',lat,lon,std_sic_masked)
+
+"""
+melt = data.variables["melt_onset_day_seaice_conc_monthly_cdr"][:]
+melt_masked = ma.masked_less_equal(melt,0.0)
+plot(fig,3,'melt onset',lat,lon,melt_masked)
+"""
+"""
+qa = data.variables["qa_of_seaice_conc_monthly_cdr"][:]
+qa_masked = ma.masked_less_equal(qa,0.0)
+plot(fig,4,'qa',lat,lon,qa_masked)
+"""
+
+g_merged = data.variables["goddard_merged_seaice_conc_monthly"][:]
+g_merged_masked = ma.masked_less_equal(g_merged,0.0)
+plot(fig,3,'goddard merged conc',lat,lon,g_merged_masked)
+
+g_nt = data.variables["goddard_nt_seaice_conc_monthly"][:]
+g_nt_masked = ma.masked_less_equal(g_nt,0.0)
+plot(fig,4,'goddard nt conc',lat,lon,g_nt_masked)
+
+g_bt = data.variables["goddard_bt_seaice_conc_monthly"][:]
+g_bt_masked = ma.masked_less_equal(g_bt,0.0)
+plot(fig,5,'goddard bt conc',lat,lon,g_bt_masked)
+
+#plt.title('South Pole Stereographic Projection for sea ice concentration for 12-2017')
+plt.subplots_adjust(wspace=0.5,hspace=0.5)
+plt.show()
