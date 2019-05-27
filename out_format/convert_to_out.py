219dfa15cf50470474f422d17048171b5197132f
Manan <manansoni0@gmail.com>
2019-05-27 15:02:20 +0530

    Fix bugs in out_format/convert_out.py Change read_data.py to be compatible with test_out.nc

--- out_format/convert_to_out.py
+++ out_format/convert_to_out.py
@@ -2,6 +2,7 @@ from netCDF4 import Dataset
 from pprint import pprint
 import numpy as np
 import numpy.ma as ma
+import matplotlib.pyplot as plt
 
 in_path = '../data/seaice_conc_monthly_sh_f08_198906_v03r01.nc'
 
@@ -14,40 +15,51 @@ root_grp.createDimension('lon', 1440)
 time = root_grp.createVariable('time', 'f8', ('time',))
 lat = root_grp.createVariable('lat', 'f4', ('lat',))
 lon = root_grp.createVariable('lon', 'f4', ('lon',))
-g_m_conc = root_grp.createVariable('goddard_merged_seaice_conc_monthly', 'f4', ('time','lat','lon'))
-m_o_day = root_grp.createVariable('melt_onset_day_seaice_conc_monthly_cdr', 'f8', ('time','lat','lon'))
+g_m_conc = root_grp.createVariable('goddard_merged_seaice_conc_monthly', 'f4', ('time','lat','lon'), fill_value=0)
+m_o_day = root_grp.createVariable('melt_onset_day_seaice_conc_monthly_cdr', 'f8', ('time','lat','lon'), fill_value=0)
 
 #in_lat = data.variables["latitude"][:]
 #in_lon = data.variables["longitude"][:]
-in_lat_fl = data.variables["latitude"][:].flatten()
-in_lon_fl = data.variables["longitude"][:].flatten()
+in_lat_fl = np.around(data.variables["latitude"][:].flatten(),3)
+in_lon_fl = np.around(data.variables["longitude"][:].flatten(),3)
+
 in_gm_fl = data.variables["goddard_merged_seaice_conc_monthly"][0,:,:].flatten()
 in_mo_fl = data.variables["melt_onset_day_seaice_conc_monthly_cdr"][0,:,:].flatten()
 
-"""
-lat[:] = in_lat[ind]
-lon[:] = in_lon[ind]
-
-print(in_lat[ind].shape)
-#print(in_lat[ind].flatten())
-#print(in_lon[ind].flatten())
-
-l = len(ind[0])
-for i in ind[0]:
-    print(f'{i}/{l}')
-    for j in ind[1]:
-        g_m_conc[0,in_lat[i,j],in_lon[i,j]] = in_gm[i,j]
-        m_o_day[0,in_lat[i,j],in_lon[i,j]] = in_mo[i,j]
-
-root_grp.close()
-"""
-
 lat[:] = np.arange(-90,-50.25,0.25)
 lon[:] = np.arange(-180,180,0.25)
-
+"""
+for i in range(-90,-50+1):
+    j = 50
+    ind = np.argmin(np.square(in_lat_fl-i)+np.square(in_lon_fl-j))
+    print(i,in_gm_fl[ind],in_gm_fl[ind].mask)
+"""
+#sic = np.zeros((159,1440))
+#counti = 0
+#countj = 0
+a = []
 for i in np.arange(-90,-50.25,0.25):
     print(i)
+    countj = 0
     for j in np.arange(-180,180,0.25):
         ind = np.argmin(np.square(in_lat_fl-i)+np.square(in_lon_fl-j))
-        g_m_conc[0,i,j] = in_gm_fl[ind]
-        m_o_day[0,i,j] = in_mo_fl[ind]
+        if ma.is_masked(in_gm_fl[ind]):
+            g_m_conc[0,i,j] = 0
+            #g_m_conc[0,i,j].mask = False
+        else:
+            g_m_conc[0,i,j] = in_gm_fl[ind]
+
+        if ma.is_masked(in_mo_fl[ind]):
+            m_o_day[0,i,j] = 0
+            #m_o_day[0,i,j].mask = False
+        else:
+            m_o_day[0,i,j] = in_mo_fl[ind]
+
+root_grp.close
+
+print('plotting....')
+print(len(a),a[0])
+for  pt in a:
+    print(pt)
+    plt.scatter(pt[0],pt[1])
+plt.show()
--- read_data.py
+++ read_data.py
@@ -5,53 +5,30 @@ import numpy.ma as ma
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import matplotlib.cm as cm
-#path = './data/seaice_conc_monthly_sh_f08_198906_v03r01.nc'
 path = './out_format/test_out.nc'
 data = Dataset(path,'r+')
+
+data.set_fill_on()
 
 for var in data.variables:
     print(var)
 print('\n')
-#print(data.variables["goddard_merged_seaice_conc_monthly"])
-#melt_onset_meta = data.variables["melt_onset_day_seaice_conc_monthly_cdr"]
-#melt_onset = data.variables["melt_onset_day_seaice_conc_monthly_cdr"][:]
-print(data.variables["lat"][:])
-print('')
-print(data.variables["lon"][:])
-print('')
-#print(data.variables["goddard_merged_seaice_conc_monthly"][:])
-#print('')
-#print(data.variables["melt_onset_day_seaice_conc_monthly_cdr"][:])
-#print(data.variables["goddard_merged_seaice_conc_monthly"][:])
-#print(melt_onset[np.where(melt_onset.mask==False)])
-"""
-def plot(fig,num,heading,lat,lon,data):
-    ax = fig.add_subplot(str(num))
-    ax.set_title(heading, pad=20)
-    # make South pole Stereographic projection
-    m = Basemap(projection='spstere', boundinglat=-30, lon_0=180., resolution='h', round=True)
 
-    # may not be required
-    #ax.title(heading)
+lat = data.variables["lat"]
+lon = data.variables["lon"]
+sic = data.variables["goddard_merged_seaice_conc_monthly"]
+print(sic)
 
-    # draw parallel longitude lines
-    m.drawmeridians(np.arange(-180.,181.,30.), labels=[1,1,1,1])
+colors = { (0,0.25):'green', (0.25,0.5): 'yellow', (0.5,0.75): 'orange', (0.75,1): 'red' }
 
-    # draw parallel latitude lines
-    m.drawparallels(np.arange(-90.,-40.,10.), labels=[1,0,0,1])
-
-    # add the observations to the map
-    m.pcolormesh(lon, lat, data[0,:,:], cmap='RdYlBu',latlon=True)
-    m.fillcontinents(alpha=0.42)
-
-    #fig.colorbar(cm.ScalarMappable(cmap='RdYlBu'),orientation='horizontal',ax=ax)
-lat = data.variables["latitude"][:]
-lon = data.variables["longitude"][:]
-fig = plt.figure()
-g_merged = data.variables["goddard_merged_seaice_conc_monthly"][:]
-g_merged_masked = ma.masked_less_equal(g_merged,0.0)
-plot(fig,111,'goddard merged conc',lat,lon,g_merged_masked)
-plt.title('South Pole Stereographic Projection for sea ice concentration for 12-2017')
-plt.subplots_adjust(wspace=0.5,hspace=0.5)
+plt.xlim([-180,180])
+plt.ylim([-90,-50])
+for i in np.arange(-90,-50+0.25,0.25):
+    print(i,sic[0,i,0])
+    for j in np.arange(-180,180+1,1):
+        if sic[0,i,j] >= 0.05:
+            for r,c in colors.items():
+                if r[0] <= sic[0,i,j] <= r[1]:
+                    plt.scatter(j,i,color=c)
+                    break
 plt.show()
-"""
